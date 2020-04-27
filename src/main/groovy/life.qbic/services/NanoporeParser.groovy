79df295b510c9178fae0cc4ca9d77c192af48c64
Steffengreiner <Steffen.Greiner@gmx.de>
2020-04-27 14:31:40 +0200

    Added File Extension property to Nanopore File Object

--- src/main/groovy/life.qbic/services/NanoporeParser.groovy
+++ src/main/groovy/life.qbic/services/NanoporeParser.groovy
@@ -2,6 +2,7 @@ import com.fasterxml.jackson.core.JsonGenerationException
 import com.fasterxml.jackson.databind.JsonMappingException
 import com.fasterxml.jackson.databind.ObjectMapper
 
+
 import javax.validation.ValidationException
 import java.nio.file.Path
 import java.nio.file.Paths
@@ -9,14 +10,16 @@ import java.nio.file.Paths
 import org.everit.json.schema.Schema;
 import org.everit.json.schema.loader.SchemaLoader;
 import org.json.JSONObject;
-import org.json.JSONTokener;
+import org.json.JSONTokener
+
+import java.util.stream.Stream;
 
 class NanoporeParser {
 
     Map<Object, Object> fileMap = new HashMap<>()
 
     class NanoporeDirectory {
-        Object children
+        ArrayList children
         Path dirPath
         String dirName
     }
@@ -24,6 +27,7 @@ class NanoporeParser {
     class NanoporeFile {
         Path filePath
         String fileName
+        String fileExtension
     }
 
     private void parseAsMap(Path filePath) {
@@ -38,15 +42,20 @@ class NanoporeParser {
             NanoporeDirectory nanoporeDirectory = new NanoporeDirectory()
             nanoporeDirectory.dirName = currentFileName
             nanoporeDirectory.dirPath = currentPathName
-            nanoporeDirectory.children = currentFile.eachFileRecurse { file ->
+            nanoporeDirectory.children = []
+            currentFile.eachFileRecurse { file ->
                 parseAsMap(Paths.get(file.path))
             }
-
             fileMap.put(nanoporeDirectory.dirName, nanoporeDirectory)
+
         } else {
             NanoporeFile nanoporeFile = new NanoporeFile()
             nanoporeFile.fileName = currentFileName
             nanoporeFile.filePath = currentPathName
+            // Get File Extension by searching for last occurrence and account for files with missing file extension
+            int i = currentFileName.lastIndexOf('.')
+            String currentFileExtension = i > 0 ? currentFileName.substring(i + 1) : ""
+            nanoporeFile.fileExtension = currentFileExtension
             fileMap.put(nanoporeFile.fileName, nanoporeFile)
         }
 
@@ -61,7 +70,7 @@ class NanoporeParser {
             return jsonFromMap
         }
         catch (JsonGenerationException e) {
-            print("Json could not be generated!")
+            print("Json could not be generated")
         } catch (JsonMappingException e) {
             print("Internal Map could not be converted to Json")
         }
@@ -69,17 +78,20 @@ class NanoporeParser {
 
     private void validateJson(String json, InputStream jsonSchemaStream) {
 
+        JSONObject jsonSchemaObject = new JSONObject(new JSONTokener(jsonSchemaStream as Stream))
         //Validation according to documentation found on https://github.com/everit-org/json-schema
         try {
-            JSONObject jsonSchemaObject = new JSONObject(new JSONTokener(jsonSchemaStream))
+
             Schema jsonSchema = SchemaLoader.load(jsonSchemaObject)
             jsonSchema.validate(json)
         }
         catch (ValidationException e) {
             print("Structure of Input Json File did not match specified Jsonschema")
+            e.printStackTrace()
         }
         catch (Exception e) {
             print("Unexpected Exception occurred")
+            e.printStackTrace()
         }
     }
 
@@ -91,9 +103,10 @@ class NanoporeParser {
         nanoporeParser.parseAsMap(testPath)
         print(nanoporeParser.fileMap)
         String json = nanoporeParser.createJson(nanoporeParser.fileMap)
+        //ToDo Inputstream returns null no matter what path is specified
         InputStream jsonSchemaStream = getClass().getResourceAsStream("/Users/steffengreiner/Documents/GitHub/Work/core-utils-lib/src/main/resources/nanopore-instrument-output.schema.json")
         nanoporeParser.validateJson(json, jsonSchemaStream)
-        print(json)
+
     }
 
 }
