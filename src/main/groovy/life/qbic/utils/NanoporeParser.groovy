295bb55caee50221e368010f5eb2207bf0e15e7a
Steffengreiner <Steffen.Greiner@gmx.de>
2020-04-27 19:33:30 +0200

    Add seperate Filesplit and Jsoncreation methods, implemented seperate constructor
    ToDo Recursion doesn't work correctly
    Inputstream always returns null object

--- src/main/groovy/life/qbic/utils/NanoporeParser.groovy
+++ src/main/groovy/life/qbic/utils/NanoporeParser.groovy
@@ -3,65 +3,118 @@ package life.qbic.utils
 import com.fasterxml.jackson.core.JsonGenerationException
 import com.fasterxml.jackson.databind.JsonMappingException
 import com.fasterxml.jackson.databind.ObjectMapper
-
-
 import javax.validation.ValidationException
 import java.nio.file.Path
 import java.nio.file.Paths
-
 import org.everit.json.schema.Schema;
 import org.everit.json.schema.loader.SchemaLoader;
 import org.json.JSONObject;
 import org.json.JSONTokener
-
 import java.util.stream.Stream;
 
 class NanoporeParser {
 
+    Path targetDirectoryPath
+    Path schemaPath
+
+    private NanoporeParser() {
+        throw new AssertionError()
+    }
+    NanoporeParser(Path targetDirectoryPath, Path schemaPath) {
+        this.targetDirectoryPath = targetDirectoryPath
+        this.schemaPath = schemaPath
+
+    }
     Map<Object, Object> fileMap = new HashMap<>()
 
     class NanoporeDirectory {
+        String name
+        Path path
         ArrayList children
-        Path dirPath
-        String dirName
+
+
+        private NanoporeDirectory() {
+            throw new AssertionError()
+        }
+
+        NanoporeDirectory(String name, Path path, List children) {
+            this.name = name
+            this.path = path
+            this.children = children
+        }
     }
 
     class NanoporeFile {
-        Path filePath
-        String fileName
-        String fileExtension
+        String name
+        Path path
+        String extension
+
+        private NanoporeFile() {
+            throw new AssertionError()
+        }
+
+        NanoporeFile(String name, Path path, String extension) {
+            this.name = name
+            this.path = path
+            this.extension = extension
+        }
     }
 
-    private void parseAsMap(Path filePath) {
+    private void directoryToMap(Path filePath) {
 
         File currentFile = new File(filePath.toString())
-        String currentFileName = currentFile.getName()
+        String currentFullFileName = currentFile.getName()
         Path currentPathName = filePath
 
         /* ToDo Find solution to map directories correctly, currently all directories are put into singular map */
-        if (currentFile.directory) {
+        if (currentFile.isDirectory()) {
 
-            NanoporeDirectory nanoporeDirectory = new NanoporeDirectory()
-            nanoporeDirectory.dirName = currentFileName
-            nanoporeDirectory.dirPath = currentPathName
-            nanoporeDirectory.children = []
+            String dirName = currentFullFileName
+            Path dirPath = currentPathName
+            List children = []
+            NanoporeDirectory nanoporeDirectory = new NanoporeDirectory(dirName, dirPath, children)
+
             currentFile.eachFileRecurse { file ->
                 nanoporeDirectory.children.add(file)
-                parseAsMap(Paths.get(file.path))
+                directoryToMap(file.toPath())
             }
-            fileMap.put(nanoporeDirectory.dirName, nanoporeDirectory)
+
+            this.fileMap.put(nanoporeDirectory.getName(), nanoporeDirectory)
 
         } else {
-            NanoporeFile nanoporeFile = new NanoporeFile()
-            nanoporeFile.fileName = currentFileName
-            nanoporeFile.filePath = currentPathName
-            // Get File Extension by searching for last occurrence and account for files with missing file extension
-            int i = currentFileName.lastIndexOf('.')
-            String currentFileExtension = i > 0 ? currentFileName.substring(i + 1) : ""
-            nanoporeFile.fileExtension = currentFileExtension
-            fileMap.put(nanoporeFile.fileName, nanoporeFile)
+
+            String[] splitFileName = fileNameSplit(currentFile)
+            String fileName = splitFileName[0]
+            String fileExtension = splitFileName[1]
+
+            NanoporeFile nanoporeFile = new NanoporeFile(fileName, currentPathName, fileExtension)
+            fileMap.put(nanoporeFile.getName(), nanoporeFile)
         }
 
+    }
+
+    private String[] fileNameSplit(File file) {
+        String fullFileName = file.getName()
+        //ToDo What about Zip Files?
+        String[] splitFileName = fullFileName.split('\\.(?=[^\\.]+$)')
+
+        String fileName
+        String fileExtension
+
+        if (splitFileName.size() > 1)
+            fileExtension = splitFileName[1]
+        else {
+            fileExtension = "None"
+        }
+        if (splitFileName[0].size() < 1) {
+            fileName = splitFileName[1]
+            fileExtension = "None"
+        } else {
+            fileName = splitFileName[0]
+        }
+
+        String[] resultingFileArray = [fileName, fileExtension]
+        return resultingFileArray
     }
 
     private String createJson(Map map) {
@@ -78,14 +131,10 @@ class NanoporeParser {
             print("Internal Map could not be converted to Json")
         }
     }
+    //Validation according to documentation found on https://github.com/everit-org/json-schema
+    private void validateJson(String json, Schema jsonSchema) {
 
-    private void validateJson(String json, InputStream jsonSchemaStream) {
-
-        JSONObject jsonSchemaObject = new JSONObject(new JSONTokener(jsonSchemaStream as Stream))
-        //Validation according to documentation found on https://github.com/everit-org/json-schema
         try {
-
-            Schema jsonSchema = SchemaLoader.load(jsonSchemaObject)
             jsonSchema.validate(json)
         }
         catch (ValidationException e) {
@@ -97,20 +146,27 @@ class NanoporeParser {
             e.printStackTrace()
         }
     }
+    //Json schema generation according to documentation found on https://github.com/everit-org/json-schema
+    private Schema fileToSchema(Path path) {
+        //ToDo InputStream returns null no matter what path is specified
+        InputStream jsonSchemaStream = getClass().getResourceAsStream(path.toString())
+        JSONObject jsonSchemaObject = new JSONObject(new JSONTokener(jsonSchemaStream as Stream))
+        Schema jsonSchema = SchemaLoader.load(jsonSchemaObject)
+        return jsonSchema
+    }
 
 /* ToDo remove!, For Testing purposes only  */
 
     static void main(String[] args) {
-        NanoporeParser nanoporeParser = new NanoporeParser()
-        Path testPath = Paths.get("/Users/steffengreiner/Desktop/testdir/")
-        nanoporeParser.parseAsMap(testPath)
-        print(nanoporeParser.fileMap)
-        String json = nanoporeParser.createJson(nanoporeParser.fileMap)
-        print(json)
-        //ToDo Inputstream returns null no matter what path is specified
-        InputStream jsonSchemaStream = getClass().getResourceAsStream("/Users/steffengreiner/Documents/GitHub/Work/core-utils-lib/src/main/resources/nanopore-instrument-output.schema.json")
-        nanoporeParser.validateJson(json, jsonSchemaStream)
+        Path dirPath = Paths.get("/Users/steffengreiner/Desktop/testdir/")
+        Path schemaPath = Paths.get("src/main/resources/nanopore-instrument-output.schema.json")
+        NanoporeParser nanoporeParser = new NanoporeParser(dirPath, schemaPath)
 
+        nanoporeParser.directoryToMap(dirPath)
+        String json = nanoporeParser.createJson(nanoporeParser.fileMap)
+
+        Schema jsonSchema = nanoporeParser.fileToSchema(schemaPath)
+        nanoporeParser.validateJson(json, jsonSchema)
     }
 
 }
