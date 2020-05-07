407eeeba5d2be2ff90d66c3800a6a2c6772d9b03
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2020-05-07 09:24:49 +0200

    Provide example Test

---
+++ src/test/groovy/life/qbic/utils/NanoporeParserSpec.groovy
@@ -0,0 +1,16 @@
+package life.qbic.utils
+
+import spock.lang.Specification
+
+class NanoporeParserSpec extends Specification{
+
+    def "Test"(){
+        when:
+        def a = "test"
+
+        then:
+        assert a == "test"
+
+    }
+
+}
