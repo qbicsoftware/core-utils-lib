b048b9dd199fc19887cf246328e29f7ca033e247
Steffengreiner <Steffen.Greiner@gmx.de>
2020-05-11 18:21:35 +0200

    Add accidentally overwritten Test for correct file structure

--- src/test/groovy/life/qbic/utils/NanoporeParserSpec.groovy
+++ src/test/groovy/life/qbic/utils/NanoporeParserSpec.groovy
@@ -8,6 +8,15 @@ class NanoporeParserSpec extends Specification {
 
     def exampleDirectoriesRoot = this.getClass().getResource("/dummyFileSystem/nanopore-instrument-output").getPath()
 
+    def "parsing a valid file structure creates a Map"() {
+        given:
+        def pathToDirectory = Paths.get(exampleDirectoriesRoot, "validates/QABCD001AB_E12A345a01_PAE12345")
+        when:
+        def map = NanoporeParser.parseFileStructure(pathToDirectory)
+        then:
+        assert map instanceof Map
+    }
+
     def "parsing an empty directory throws NullPointerException"() {
         given:
         def pathToDirectory = Paths.get(exampleDirectoriesRoot, "/empty_directory")
