8a0c00e5599c61feb6383e6a76d7c0ba4230d7e2
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2020-06-05 15:58:24 +0200

    Provide class that handles registration

---
+++ src/main/groovy/life/qbic/dss/registration/RegisterDatasetWithMetadata.groovy
@@ -0,0 +1,18 @@
+package life.qbic.dss.registration
+
+import ch.systemsx.cisd.etlserver.registrator.api.v2.IDataSetRegistrationTransactionV2
+
+/**
+ * Orchestration of dataset registration with metadata
+ *
+ *
+ *
+ * @author: Sven Fillinger
+ */
+class RegisterDatasetWithMetadata {
+
+    RegisterDatasetWithMetadata(IDataSetRegistrationTransactionV2 transaction) {
+
+    }
+
+}
