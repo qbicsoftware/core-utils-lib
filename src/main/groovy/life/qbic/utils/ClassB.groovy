3b2a359cc403e1f62b9759fa227f224a1bc4946e
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2020-09-21 10:22:53 +0200

    PRovide test classes for core-utils

---
+++ src/main/groovy/life/qbic/utils/ClassB.groovy
@@ -0,0 +1,17 @@
+package life.qbic.utils
+
+/**
+ * <class short description - 1 Line!>
+ *
+ * <More detailed description - When to use, what it solves, etc.>
+ *
+ * @author Sven Fillinger
+ * @since <versiontag>
+ */
+class ClassB {
+
+  ClassB(ClassA a) {
+
+  }
+
+}
