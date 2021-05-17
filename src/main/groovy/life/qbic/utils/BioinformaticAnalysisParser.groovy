eeb71beb025612b5fb56a305a383e4737838d018
Steffengreiner <Steffen.Greiner@gmx.de>
2021-05-17 10:39:55 +0200

    Remove empty line from BioinformaticAnalysisParser
    Code review suggestion
    
    Co-authored-by: Sven F. <sven.fillinger@qbic.uni-tuebingen.de>

--- src/main/groovy/life/qbic/utils/BioinformaticAnalysisParser.groovy
+++ src/main/groovy/life/qbic/utils/BioinformaticAnalysisParser.groovy
@@ -21,7 +21,6 @@ import java.text.ParseException
  * @param directory path of nf-core directory whose fileTree should be converted into a JSON String
  *
  */
-
 @Log4j2
 class BioinformaticAnalysisParser implements DatasetParser<NfCorePipelineResult>{
 
